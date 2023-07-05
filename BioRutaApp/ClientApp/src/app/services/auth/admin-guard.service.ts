147f975dfe8954c5e57036726a87112655728ecc
DARP\VASK1M7R <Vass2022>
2023-07-04 21:50:24 -0500

    Validacion de acceso

---
+++ BioRutaApp/ClientApp/src/app/services/auth/admin-guard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { User } from "src/app/models/User";
+import { Controllers } from "src/environments/environment";
+import { DataService } from "../data.service";
+import { SecurityService } from "../security.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+
+  constructor(private router: Router, private securityService: SecurityService, private dataService: DataService){}
+
+  canActivate(): boolean {
+    if(this.securityService.IsAuth){
+      return true;
+    }else{
+      this.router.navigate(["/login"]);
+    }
+    return false;
+  }
+}
